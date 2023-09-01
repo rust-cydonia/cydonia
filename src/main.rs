ff189fb1eb72895d2d48a9e7c66508f42ca9cc5c
nussun-celestia <69641920+nussun-celestia@users.noreply.github.com>
2023-09-01 09:15:34 +0200

    Change to vsop87a

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::time::UNIX_EPOCH;
-use vsop87::vsop87c;
+use vsop87::vsop87a;
 
 fn main() {
     let t = UNIX_EPOCH.elapsed().unwrap().as_nanos() as i128;
     let jd = t as f64 / (86400.0 * 1000000000.0) + 2440587.5;
-    println!("{:?}", vsop87c::mercury(jd));
-    println!("{:?}", vsop87c::venus(jd));
-    println!("{:?}", vsop87c::earth(jd));
-    println!("{:?}", vsop87c::mars(jd));
-    println!("{:?}", vsop87c::jupiter(jd));
-    println!("{:?}", vsop87c::saturn(jd));
-    println!("{:?}", vsop87c::uranus(jd));
-    println!("{:?}", vsop87c::neptune(jd));
+    println!("{:?}", vsop87a::mercury(jd));
+    println!("{:?}", vsop87a::venus(jd));
+    println!("{:?}", vsop87a::earth(jd));
+    println!("{:?}", vsop87a::mars(jd));
+    println!("{:?}", vsop87a::jupiter(jd));
+    println!("{:?}", vsop87a::saturn(jd));
+    println!("{:?}", vsop87a::uranus(jd));
+    println!("{:?}", vsop87a::neptune(jd));
 }
